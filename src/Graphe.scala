63cf5dbe0d3415747873d22e8b01fe7530bb5775
Florent <florent.delgrange@gmail.com>
2015-12-02 15:26:27 +0100

    Dijkstra works with all graph. You can test it in Main with testDijkstra()

--- src/Graphe.scala
+++ src/Graphe.scala
@@ -1,28 +1,29 @@
 class Graphe(val head: Array[Int], val succ: Array[Int], val dist: Array[Int], val prospectus: Array[Int]){
-  
-  val D = Array.fill[Array[Int]](head.length)(Array.fill[Int](head.length)(Int.MaxValue))
+
+  val D = Array.fill[Array[Int]](head.length -1 )(Array.fill[Int](head.length -1 )(Int.MaxValue))
 
   def relax(s: Int, u: Int, i: Int){
-    if( D(s)(succ(i)) > (D(s)(u) + dist(i)) )
+    if( ( D(s)(u) + dist(i) ) >= 0 && D(s)(succ(i)) > (D(s)(u) + dist(i)) )
+      //First condition to avoid the overflow in case of D(s)(u) or dist(i) = infinity
       D(s)(succ(i)) = D(s)(u) + dist(i)
   }
 
   def dijkstra(s: Int){
     D(s)(s) = 0
-    var N = Set(0)
-    while(N.size != head.length){
+    var N = Set(0, (head.length-1))
+    while(N.size < head.length){
       var u = Int.MaxValue
       for( i <- D(s).indices.filter({x: Int => !(N.contains(x))}) )
-        if(u < D(s)(i))
-          u = D(s)(i)
+        if(u >= D(s)(i))
+          u = i
       N = N+u
       for( i <- head(u) to (head(u+1) -1) )
         relax(s, u, i)
     }
   }
-  
+
   def fullDijkstraUpdate(){
-    for( i <- 1 to (head.length -1) )
+    for( i <- 1 to (head.length -2) )
       dijkstra(i)
   }
 
---
+++ src/Main.scala
@@ -0,0 +1,17 @@
+object Main {
+
+  def main(args: Array[String]){
+    val graph = new Graphe(Array(0,1,4,5,7,11,14,17,18,19), Array(0,2,4,6,3,4,8,2,5,6,7,3,7,8,4,5,7,8,7,0), Array(0,1,5,7,3,0,8,1,1,3,9,1,4,7,2,2,4,2,1,0), Array())
+    graph.fullDijkstraUpdate()
+    dijkstraTest(graph.D)
+  }
+
+  def dijkstraTest(D: Array[Array[Int]]): Unit ={
+    for(i<- 1 to D.length -1) {
+      println("Plus courts chemis du sommet ", i, " à : ")
+      for (j <- 1 to D(i).length - 1)
+        println("---> ", j, " = ", D(i)(j))
+    }
+  }
+
+}
