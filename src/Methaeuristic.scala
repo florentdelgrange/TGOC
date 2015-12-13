e052bcb0f140f8871168967db082e459c0b47971
radioGiorgio <florent.delgrange@gmail.com>
2015-12-13 21:37:41 +0100

    gros j'ai oublié de virer les lignes 152 et 153

--- src/Methaeuristic.scala
+++ src/Methaeuristic.scala
@@ -149,8 +149,6 @@ object Methaeuristic {
       var CCs = Array[graph.CC]()
       for(i<- 1 to maxIter){
         var array = greedy_proba(step * i)
-        array.foreach(cc => println("CC : " + computeEdges(cc)))
-        array.foreach(cc => println("Prospectus : "+ cc.prospectusCovered + ", distance : " + cc.distCovered + ", compacity : "+ Methaeuristic.compacity(cc) + " Error : " + graphCost(cc, array)))
         if(score(array) <= CCsScore) {
           println("Meilleures composantes connexes trouvées : essai " + i)
           array.foreach(cc => println("CC : " + computeEdges(cc)))
